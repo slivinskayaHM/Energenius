87b39d08deb0aca82c5c7e692f09334e95d1310e
slivinskayaHM <a.slivinskaya@hm.edu>
2023-02-07 16:22:02 +0100

    HashMap Sitzt / Zahnstocher spitz

--- Energenius/src/main/java/Runner.java
+++ Energenius/src/main/java/Runner.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class Runner {
     ArrayList<Fahrt> Fahrten;
-    //Prognosedaten[][];
+    HashMap<Date, Double> Prognosedaten;
     Realdatum[][] Realdaten;
     public static void main(String[] args) {
 
--- Energenius/src/main/java/Solaranlage.java
+++ Energenius/src/main/java/Solaranlage.java
@@ -1,7 +1,13 @@
+import java.util.Date;
+import java.util.HashMap;
+
 public class Solaranlage {
     double gegenwärtigeStromzeugung;
-    //Prognosedaten[][];
+    HashMap<Date, Double> Prognosedaten;
 
+    public Solaranlage(HashMap<Date, Double> Prognosedaten) {
+        this.Prognosedaten = Prognosedaten;
+    }
     boolean shouldCharge(Realdatum Uhrzeit, double gegenwärtigeStromzeugung) {
         return false;
     }
