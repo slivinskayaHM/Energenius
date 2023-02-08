6784542462be0a27a8a0bfa8bb89e2eebbb8e0c5
slivinskayaHM <a.slivinskaya@hm.edu>
2023-02-08 09:19:35 +0100

    Constructor Auto

--- Energenius/src/main/java/Auto.java
+++ Energenius/src/main/java/Auto.java
@@ -2,4 +2,11 @@ public class Auto {
     double Akkustand;
     double maxAkku;
     String id;
+    Fahrplan fahrplan;
+    public Auto(double maxAkku, String id, Fahrplan fahrplan) {
+        this.maxAkku = maxAkku;
+        this.id = id;
+        this.fahrplan = fahrplan;
+        this.Akkustand = 0;
+    }
 }
